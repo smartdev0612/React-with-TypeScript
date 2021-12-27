eb00b98c8394486922f7e695f81be6a5873d0b02
jixu1031 <goodluck1031@outlook.com>
2021-12-28 05:08:39 +0900

    Create Add List using react.js and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from "./components/List";
+import AddToList from './components/AddToList';
+
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
 
 function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgcI_0viFjmGV9FK9Upo6ynixtatl_mXrfpg&usqp=CAU",
+      age: 36,
+      note: "Allegeric to staying on the same team"
+    },
+    {
+      name: "Kobe Bryant",
+      url: "https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F200824175931-kobe-bryant-file.jpg",
+      age: 36
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited to my Party</h1>
+      <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
